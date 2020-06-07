f31d164dac0937e38365c222ffc7721724cd3f9b
EtheroX <ejsuhr@hotmail.com>
2020-06-07 16:23:30 +1000

    7/6/2020 4:23 PM
    Main Menu Completion

---
+++ Tank Game/Assets/Scripts/UI/Menu/MenuUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    public GameObject exitMenu;
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void ExitMenu(bool exiting)
+    {
+        exitMenu.SetActive(exiting);
+    }
+}
